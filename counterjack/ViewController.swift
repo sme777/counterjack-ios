25d5126f9792957861a776870a57122a8df0d212
mr-sm3 <sm3@berkeley.edu>
2020-11-08 16:12:05 +0400

    Initial Commit

---
+++ counterjack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  counterjack
+//
+//  Created by Samson Petrosyan on 11/8/20.
+//  Copyright © 2020 Samson Petrosyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
