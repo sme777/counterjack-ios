16e29ba326c9b727d685e3f4ced95cd261cd8820
mr-sm3 <sm3@berkeley.edu>
2020-11-19 00:08:44 +0400

    start AI heursitic

--- counterjack/Models/AI.swift
+++ counterjack/Models/AI.swift
@@ -12,40 +12,161 @@ import Foundation
 class AI {
     
     private var betChoice: Int
+    private var doubleAfterSplit: Bool
+    private var bankroll: Double
     private var currentCards: Array<Card> = []
     private var dealerCards: Array<Card> = []
     
-    init(betChoice: Int) {
-        self.betChoice = betChoice
+    //MARK: - AI initializers
+
+    init() {
+        self.betChoice = 1
+        self.doubleAfterSplit = true
+        self.bankroll = 5000
     }
     
-    func addCurrentCard(_ card: Card) {
+    init(betChoice: Int) {
+        self.betChoice = betChoice
+        self.doubleAfterSplit = true
+        self.bankroll = 5000
+    }
+    
+    init(betChoice: Int, doubleAfterSplit: Bool) {
+        self.betChoice = betChoice
+        self.bankroll = 5000
+        self.doubleAfterSplit = doubleAfterSplit
+    }
+    
+    
+    init(betChoice: Int, bankroll: Double) {
+        self.betChoice = betChoice
+        self.bankroll = bankroll
+        self.doubleAfterSplit = true
+    }
+    
+    init(betChoice: Int, doubleAfterSplit: Bool, bankroll: Double) {
+        self.betChoice = betChoice
+        self.bankroll = bankroll
+        self.doubleAfterSplit = doubleAfterSplit
+    }
+    
+    public func addCurrentCard(_ card: Card) {
         currentCards.append(card)
     }
     
-    func addDealerCard(_ card: Card) {
+    public func addDealerCard(_ card: Card) {
         dealerCards.append(card)
     }
     
-    func removeCard(_ card: Card) {
+    public func removeCard(_ card: Card) {
         let cardIndex = currentCards.firstIndex{$0.face == card.face && $0.suit == card.suit}
         if let cardPosition = cardIndex {
             currentCards.remove(at: cardPosition)
         }
     }
     
-    func removeDealerCard(_ card: Card) {
+    public func removeDealerCard(_ card: Card) {
         let cardIndex = dealerCards.firstIndex{$0.face == card.face && $0.suit == card.suit}
         if let cardPosition = cardIndex {
             dealerCards.remove(at: cardPosition)
         }
     }
     
-    func removeAllCards() {
+    public func removeAllCards() {
         currentCards.removeAll()
     }
     
-    func removeAllDealerCards() {
+    public func removeAllDealerCards() {
         dealerCards.removeAll()
     }
+    
+    public func move() -> String? {
+        if !isValid() {
+            return nil
+        } else if currentCards[0].face == currentCards[1].face {
+            return split()
+        } else if isSoft() {
+            if isSurrender() {
+                return "surrender"
+            } else {
+                return soft()
+            }
+        } else {
+            if isSurrender() {
+                return "surrender"
+            } else {
+                return hard()
+            }
+        }
+    }
+    
+    
+    private func isValid() -> Bool {
+        return currentCards.count == 2 && dealerCards.count == 1
+    }
+    
+    //check if the sum exceeds 21 for Ace
+    private func isSoft() -> Bool {
+        for i in 0..<currentCards.count {
+            if (currentCards[i].face == "A") {
+                return true
+            }
+        }
+        return false
+    }
+    
+    private func isSurrender() -> Bool {
+        let sum = count(currentCards)
+        if sum == 16 && !isSoft() && inRange(from: "9", to: "A") {
+            return true
+        } else if sum == 15  && !isSoft() && inRange(from: "10", to: "A") {
+            return true
+        } else {
+            return false
+        }
+    }
+    // TODO: splits cards
+    private func split() -> String {
+        return ""
+    }
+    // TODO: moves on soft cards
+    private func soft() -> String {
+        return ""
+    }
+    
+    // TODO: moves on hard cards
+    private func hard() -> String {
+        return ""
+    }
+    // TODO: counts hand cards
+    private func count(_ cards: Array<Card>) -> Int {
+        return 0
+    }
+    
+    // TODO: check is any of the cards in range is Dealers card
+    private func inRange(from: String, to: String) -> Bool {
+        return false
+    }
+    // MARK: - Getter methods
+    
+    public func getCurrentCards() -> Array<Card> {
+        return self.currentCards
+    }
+    
+    public func getDealerCards() -> Array<Card> {
+        return self.dealerCards
+    }
+    
+    public func getBankroll() -> Double {
+        return self.bankroll
+    }
+    
+    public func getBetChoice() -> Int {
+        return self.betChoice
+    }
+    
+    public func getDoubleAfterSplit() -> Bool {
+        return self.doubleAfterSplit
+    }
+    
 }
--- counterjackTests/AITests.swift
+++ counterjackTests/AITests.swift
@@ -12,15 +12,131 @@ import XCTest
 
 class AITests: XCTestCase {
     
-    func testAddingCard() {
-        
+    
+    func testMakeDefaultAI() {
+        let ai = AI()
+        XCTAssertEqual(ai.getBetChoice(), 1)
+        XCTAssertEqual(ai.getBankroll(), 5000)
+        XCTAssertEqual(ai.getDoubleAfterSplit(), true)
     }
     
-    func testRemovingCard() {
-        
+    func testCustomBetChoiceAI() {
+        let ai = AI(betChoice: 2)
+        XCTAssertEqual(ai.getBetChoice(), 2)
+        XCTAssertEqual(ai.getBankroll(), 5000)
+        XCTAssertEqual(ai.getDoubleAfterSplit(), true)
+    }
+    
+    func testCustomBetChoiceAndBankrollAI() {
+        let ai = AI(betChoice: 3, bankroll: 2500)
+        XCTAssertEqual(ai.getBetChoice(), 3)
+        XCTAssertEqual(ai.getBankroll(), 2500)
+        XCTAssertEqual(ai.getDoubleAfterSplit(), true)
+    }
+    
+    
+    func testCustomBetChoiceAndSplitAI() {
+        let ai = AI(betChoice: 1, doubleAfterSplit: false)
+        XCTAssertEqual(ai.getBetChoice(), 1)
+        XCTAssertEqual(ai.getBankroll(), 5000)
+        XCTAssertEqual(ai.getDoubleAfterSplit(), false)
+    }
+    
+    func testCustomBetChoiceAndBankrollAndSplitAI() {
+        let ai = AI(betChoice: 10, doubleAfterSplit: true, bankroll: 10000)
+        XCTAssertEqual(ai.getBetChoice(), 10)
+        XCTAssertEqual(ai.getBankroll(), 10000)
+        XCTAssertEqual(ai.getDoubleAfterSplit(), true)
+    }
+    
+    
+    func testAddingCard() {
+        let ai = AI()
+        let card1 = Card(suit: "H", face: "A")
+        let card2 = Card(suit: "S", face: "2")
+        ai.addCurrentCard(card1)
+        ai.addDealerCard(card2)
+        XCTAssertEqual(ai.getCurrentCards().first, card1)
+        XCTAssertEqual(ai.getDealerCards().first, card2)
+        XCTAssertEqual(ai.getCurrentCards().count, 1)
+        XCTAssertEqual(ai.getDealerCards().count, 1)
+    }
+    
+    func testRemovingAICards() {
+        let ai = AI()
+        let card1 = Card(suit: "H", face: "A")
+        let card2 = Card(suit: "S", face: "2")
+        ai.addCurrentCard(card1)
+        ai.addCurrentCard(card2)
+        XCTAssertEqual(ai.getCurrentCards()[0], card1)
+        XCTAssertEqual(ai.getCurrentCards()[1], card2)
+        ai.removeCard(card1)
+        XCTAssertEqual(ai.getCurrentCards().count, 1)
+        XCTAssertEqual(ai.getCurrentCards()[0], card2)
+        ai.removeCard(card2)
+        XCTAssertEqual(ai.getCurrentCards().count, 0)
+    }
+    
+    func testRemovingDealerCards() {
+        let ai = AI()
+        let card1 = Card(suit: "C", face: "A")
+        let card2 = Card(suit: "D", face: "9")
+        ai.addDealerCard(card1)
+        ai.addDealerCard(card2)
+        XCTAssertEqual(ai.getDealerCards()[0], card1)
+        XCTAssertEqual(ai.getDealerCards()[1], card2)
+        ai.removeDealerCard(card1)
+        XCTAssertEqual(ai.getDealerCards().count, 1)
+        XCTAssertEqual(ai.getDealerCards()[0], card2)
+        ai.removeDealerCard(card2)
+        XCTAssertEqual(ai.getDealerCards().count, 0)
     }
     
     func testRemovingAllCards() {
+        let ai = AI()
+        let card1 = Card(suit: "C", face: "A")
+        let card2 = Card(suit: "D", face: "9")
+        ai.addDealerCard(card1)
+        ai.addDealerCard(card2)
+        let card3 = Card(suit: "C", face: "A")
+        let card4 = Card(suit: "D", face: "9")
+        ai.addCurrentCard(card3)
+        ai.addCurrentCard(card4)
+        ai.removeAllCards()
+        XCTAssertEqual(ai.getCurrentCards().count, 0)
+        ai.removeAllDealerCards()
+        XCTAssertEqual(ai.getDealerCards().count, 0)
+    }
+    
+    func testCorrectSplit() {
+        
+    }
+    
+    func testIncorrectSplit() {
+        
+    }
+    
+    func testCorrectSoftMove() {
+        
+    }
+    
+    func testIncorrectSoftMove() {
+        
+    }
+    
+    func testCorrectHardMove() {
+        
+    }
+    
+    func testIncorrectHardMove() {
+        
+    }
+    
+    func testCorrectSurrenderMove() {
+        
+    }
+    
+    func testIncorrectSurrenderMove() {
         
     }
 }
